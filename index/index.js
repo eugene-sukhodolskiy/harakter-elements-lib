670c07d73fa3f16a2d755f66b5cb96f1aad4ca47
Eugene <goldmikki@gmail.com>
2020-02-22 21:05:10 +0200

    Make html/css navigation on library
    issue https://github.com/eugene-sukhodolskiy/harakter-elements-lib/issues/1

--- index/index.js
+++ index/index.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
 	console.log('Ready App');
+	tabControl();
 });
+
+let currentTabID;
+
+function tabControl(){
+	setInterval(function(){
+		let hash = document.location.hash;
+		let tabID = hash.split('#tab=')[1];
+		if(typeof tabID != 'undefined'){
+			if(currentTabID != tabID){
+				let tab = $('#' + tabID);
+				if(!tab.hasClass('active')){
+					$('.tab.active').removeClass('active');
+					$('[data-tab].active').removeClass('active');
+					
+					$('#' + tabID).addClass('active');
+					$('[data-tab="' + tabID + '"]').addClass('active');
+				}
+				currentTabID = tabID;
+			}
+		}else{
+			// default tab
+		}
+	}, 50);
+}
